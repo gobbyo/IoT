dfc9519fb00f043e33944db4bdbbf94b48f2dc63
Jeff Beman <jbeman@microsoft.com>
2022-11-23 13:38:51 -0800

    created new MQTT python scripts

--- utilities/maproute.py
+++ utilities/maproute.py
@@ -16,7 +16,7 @@ client = MapsRouteClient(
 )
 
 fp = open("data.json","w")
-result = client.get_route_directions(route_points=[(48.503364324046956, -122.66808799216393), (47.245622, -122.429602)], 
+result = client.get_route_directions(route_points=[(47.61796662516246,-121.949074626174), (47.647353510681015,-121.91641447009559)], 
 travel_mode="car", vehicle_engine_type="electric", compute_travel_time="all",
 constant_speed_consumption_in_kw_h_per_hundred_km="50,8.2:130,21.3", current_charge_in_kw_h=80, max_charge_in_kw_h=80)
 i = 0
