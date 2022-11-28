76091b6dca5687467e360be80585c40c378c2fd7
Jeff Beman <jbeman@microsoft.com>
2022-11-28 13:05:44 -0800

    updated directory structure, changed device listen

--- python/iothubc2d.py
+++ python/iothubc2d.py
@@ -1,6 +1,7 @@
 import uuid
 import sys
 import asyncio
+import pickle
 from azure.iot.hub import IoTHubRegistryManager
 
 async def main():
@@ -9,17 +10,22 @@ async def main():
 
     conn_str = sys.argv[1]
     deviceId = sys.argv[2]
-
+    route = []
+    
     try:
         uniqueuserId = input("your user ID:")
         mapkey = input("subscription key to map service:")
-        startLoc = input("start location:").replace(" ","")
-        endLoc = input("end location:").replace(" ","")
+        maptype = input("'guidance' or 'route' map type:")
+        waypoints = input("number of waypoints (2 or more): ")
+        i = 0
+        while i < int(waypoints):
+            route.append(input("waypoint {0}:".format(str(i))).replace(" ",""))
+            i += 1
 
         # Create IoTHubRegistryManager
         registry_manager = IoTHubRegistryManager(conn_str)
 
-        data = ("{0}:{1}").format(startLoc,endLoc)
+        data = (pickle.dumps(route))
 
         props={}
         # optional: assign system properties
@@ -29,7 +35,7 @@ async def main():
 
         props.update(key = str(mapkey))
         props.update(userId = str(uniqueuserId))
-        props.update(mapType = "route")
+        props.update(mapType = maptype)
 
         registry_manager.send_c2d_message(deviceId, data, properties=props)
 
---
+++ python/maproute.py
@@ -0,0 +1,100 @@
+import os
+import string
+import json
+import sys
+import azure.maps.route.models
+from azure.core.credentials import AzureKeyCredential
+from azure.maps.route import MapsRouteClient
+
+#listOfLatLon = listOfLatLon.append('47.61796662516246, -121.949074626174'), listOfLatLon.append('47.647353510681015, -121.91641447009559'), etc.
+def createRouteList(listOfLatLon):
+    latlonroute = []
+    i = 0
+    while i < len(listOfLatLon):
+        point = listOfLatLon[i].split(',')
+        latlonroute.append( azure.maps.route.models.LatLon(point[0].replace(' ',''),point[1].replace(' ','')))
+        i += 1
+    return latlonroute
+
+#mapkey: Azure Maps authentication primary key
+#route: list of LatLon
+#filepath: writes json snippets to file if exists
+def printRouteOfLatLon(mapkey,route,filepath):
+    credential = AzureKeyCredential(mapkey)
+
+    client = MapsRouteClient(
+        credential=credential
+    )
+
+    result = client.get_route_directions(route_points=route, 
+    travel_mode="car", instructions_type="text", route_type="shortest", vehicle_engine_type="electric", compute_travel_time="all",
+    constant_speed_consumption_in_kw_h_per_hundred_km="50,8.2:130,21.3", current_charge_in_kw_h=80, max_charge_in_kw_h=80)
+
+    print("--Route LatLon List--")
+
+    pt = 0
+    leg = 0
+
+    if(os.path.exists(filepath)):
+        fp = open(filepath,"w")
+
+        while leg < len(result.routes[0].legs):
+            while pt < len(result.routes[0].legs[leg].points):
+                fp.write("{" + "{0}, {1}".format(str(result.routes[0].legs[0].points[pt].latitude), str(result.routes[0].legs[0].points[pt].longitude)) + "}\n")
+                pt += 1
+            leg += 1
+
+        fp.close()
+    else:
+        while leg < len(result.routes[0].legs):
+            while pt < len(result.routes[0].legs[leg].points):
+                print("{lat:" + str(result.routes[0].legs[0].points[pt].latitude) + ",lon:" + str(result.routes[0].legs[0].points[pt].longitude) + "}")
+                pt += 1
+            leg += 1
+
+#guidance: see Azure map result.routes[].guidance
+#i: index of guidance.instructions
+#total: total guidance.instructions
+def guidanceInstructions(guidance, i, total):
+    s = ''
+    if(guidance.instructions[i].message == 'None'):
+        if(i < total-1):
+            s = "{0}, then drive for {1} meters".format(guidance.instructions[i].combined_message, guidance.instructions[i+1].route_offset_in_meters - guidance.instructions[i].route_offset_in_meters)
+        else:
+            s = guidance.instructions[i].combined_message
+    else:
+        if(i < total-1):
+            s = "{0}, then drive for {1} meters".format(guidance.instructions[i].message, guidance.instructions[i+1].route_offset_in_meters - guidance.instructions[i].route_offset_in_meters)
+        else:
+            s = guidance.instructions[i].message
+    return s
+
+#mapkey: Azure Maps authentication primary key
+#route: list of LatLon
+#filepath: writes to file if exists
+def printRouteGuidanceLatLon(mapkey,route,filepath):
+    
+    credential = AzureKeyCredential(mapkey)
+
+    client = MapsRouteClient(
+        credential=credential
+    )
+
+    result = client.get_route_directions(route_points=route, 
+    travel_mode="car", instructions_type="text", route_type="shortest", vehicle_engine_type="electric", compute_travel_time="all",
+    constant_speed_consumption_in_kw_h_per_hundred_km="50,8.2:130,21.3", current_charge_in_kw_h=80, max_charge_in_kw_h=80)
+    
+    print("--Route Guidance--")
+
+    i = 0
+    total = len(result.routes[0].guidance.instructions)
+
+    if(os.path.exists(filepath)):
+        fp = open(filepath, 'w')
+        while i < total:
+            fp.write(guidanceInstructions(result.routes[0].guidance, i, total) + "\n")
+            i += 1
+    else:
+        while i < total:
+            print(guidanceInstructions(result.routes[0].guidance, i, total))
+            i += 1
