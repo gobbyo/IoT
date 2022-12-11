5b5682b72373b0ae4a1fd576b63dae49a557c917
Jeff Beman <jbeman@microsoft.com>
2022-12-11 09:57:41 -0800

    updated message content

--- python/d2csensehat.py
+++ python/d2csensehat.py
@@ -30,7 +30,9 @@ def get_stats(sense, client):
     print(b)
     sense.show_message(b,0.05,color,background)
 
-    msg = json.dumps('{ "msgsent_utc":"{0}", "fahrenheit":"{1}", "humdity":"{2}", "pressure":"{3}" }'.format(datetime.utcnow().isoformat(),t,h,b))
+    msg = Message(json.dumps('{ "msgsent_utc":"{0}", "fahrenheit":"{1}", "humdity":"{2}", "pressure":"{3}" }'.format(datetime.utcnow().isoformat(),t,h,b)))
+    msg.content_type = 'application/json'
+    msg.content_encoding = 'utf-8'
     client.send_message(msg)
 
 def main():
