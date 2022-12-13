14c6ead520c1abfd233591337e9ef7a5f3e064c8
Jeff Beman <jbeman@microsoft.com>
2022-12-12 20:01:54 -0800

    added lumination increase

--- python/simpleLED.py
+++ python/simpleLED.py
@@ -1,32 +1,43 @@
-from gpiozero import LED
+from gpiozero import PWMLED
 import time
 
-red = LED(12)
-green = LED(6)
-blue= LED(10)
+red = PWMLED(12)
+green = PWMLED(6)
+blue= PWMLED(10)
 
+luminIncr = 0.05
 i = 0
 j = 0
+k = 0.0
+direction = True
 
 while True:
     if j == 0:
-        red.on()
+        red.value = k
         green.off()
         blue.off()
     elif j == 1:
         red.off()
-        green.on()
+        green.value = k
         blue.off()
     else:
         red.off()
         green.off()
-        blue.on()
+        blue.value = k
     time.sleep(0.25)
     i += 1
     j += 1
     if j > 2:
         j = 0
 
+    if direction == True:
+        k += luminIncr
+        if k >= 1.0:
+            direction == False
+    else:
+        k -= luminIncr
+        if k <= 0:
+            direction == True
 red.off()
 green.off()
 blue.off()
