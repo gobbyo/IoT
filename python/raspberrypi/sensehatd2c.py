0ab5505dd19d665791b6ecd9c98931ac46f8fd10
Jeff Beman <jbeman@microsoft.com>
2022-12-16 12:52:44 -0800

    removed logging

--- python/raspberrypi/sensehatd2c.py
+++ python/raspberrypi/sensehatd2c.py
@@ -3,9 +3,7 @@ from decouple import config
 from azure.iot.device import IoTHubDeviceClient
 from azure.iot.device import exceptions
 from sense_hat import SenseHat
-from datetime import datetime, timezone
-import os
-import logging
+
 
 
 def main():
@@ -16,12 +14,10 @@ def main():
         f = (sense.temperature * 9/5) + 32
         msg = '{ "sent_utc":"%sZ", "fahrenheit":"%3.0f", "humidity":"%3.0f", "pressure":"%3.0f" }'%(datetime.utcnow().isoformat(),f,sense.humidity,sense.pressure)
         client.send_message(msg)
-        
-        path = os.getcwd() + '//log{0}.txt'.format(datetime.now(timezone.utc).strftime("%Y%m%d"))
-        logging.basicConfig(filename=path, filemode='a', level=logging.DEBUG)
-        logging.info(msg)
     finally:
         # Graceful exit
+        print(msg)
+        print("-------")
         sense.clear()
 
 if __name__ == "__main__":
