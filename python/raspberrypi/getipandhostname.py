57c9bc21969e6142be9c2b8189477aeb4ca70d2d
jbeman <jbeman@hotmail.com>
2022-12-19 13:01:25 -0800

    added more logging

--- python/raspberrypi/getipandhostname.py
+++ python/raspberrypi/getipandhostname.py
@@ -3,12 +3,14 @@ from decouple import config
 from azure.iot.device import IoTHubDeviceClient, exceptions
 
 def get_ip_address():
- ip_address = '';
- s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
- s.connect(("8.8.8.8",80))
- ip_address = s.getsockname()[0]
- s.close()
- return ip_address
+    print("enter get_ip_address()")
+    ip_address = '';
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8",80))
+    ip_address = s.getsockname()[0]
+    s.close()
+    print("returning ip_address()")
+    return ip_address
 
 def main():
     try:
@@ -16,12 +18,11 @@ def main():
         ip_address = get_ip_address()
 
         msg = '{{ "Hostname:{0}", "IPAddress:{1}" }}'.format(hostname, ip_address)
-        print("Hostname: {0}".format(hostname))
-        print("IP address: {0}".format(ip_address))
+        print(msg)
         client = IoTHubDeviceClient.create_from_connection_string(config("IOTHUB_DEVICE_CONNECTION_STRING"))
         client.send_message(msg)
     finally:
-        print("complete")
+        print("complete getipandhostname.py")
 
 if __name__ == "__main__":
     main()
