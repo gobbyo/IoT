34680e46d33dad16310a62241cc26111c5a786a7
Jeff Beman <jbeman@microsoft.com>
2023-01-01 16:16:52 -0800

    updated

--- python/c2dsendledbar.py
+++ python/c2dsendledbar.py
@@ -4,24 +4,22 @@ import os
 
 def main():
     deviceId = input("Device id: ")
-    s = input("On or Off: ")
     # Add any json to the payload as the content type is set to json
-    payload = '{ "remote call": "Update LED" }'
     props={}
 
     try:
-        registry_manager = IoTHubRegistryManager(os.getenv("IOTHUB_CONNECTION_STRING"))
-        
+        registry_manager = IoTHubRegistryManager(os.getenv('IOTHUB_CONNECTION_STRING'))
         # assign system properties
         props.update(messageId = "{0}".format(uuid4()))
+        props.update(contentEncoding = "utf-8")
         props.update(contentType = "application/json")
-        # assign 
-        props.update(LEDBar=True)
-        payload = '{ "iter":"1", "order":"0,1,2,3,4,5,6,7,8,9", "barpause":".125", "repeatpause":"0.5" }'
+
+        #payload = '{ "state":"on", "order":["0","1","2","3","4","5","6","7","8","9"],"pause":".25" }'
+        payload = '{ "state":"off", "order":["5","7","1","3","0","4","2","9","8","6"],"pause":".25" }'
 
         registry_manager.send_c2d_message(deviceId, payload, props)
     except Exception as ex:
-        print ( "Unexpected error {0}" % ex )
+        print ( "Unexpected error {0}".format(ex) )
 
 if __name__ == "__main__":
     main()
