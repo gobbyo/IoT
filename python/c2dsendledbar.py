ea5f207b8b7d3d14411cd63fd04806d5b52d8f4f
Jeff Beman <jbeman@microsoft.com>
2023-01-01 16:18:31 -0800

    minor fixes

--- python/c2dsendledbar.py
+++ python/c2dsendledbar.py
@@ -14,8 +14,8 @@ def main():
         props.update(contentEncoding = "utf-8")
         props.update(contentType = "application/json")
 
-        #payload = '{ "state":"on", "order":["0","1","2","3","4","5","6","7","8","9"],"pause":".25" }'
-        payload = '{ "state":"off", "order":["5","7","1","3","0","4","2","9","8","6"],"pause":".25" }'
+        payload = '{ "state":"on", "order":["0","1","2","3","4","5","6","7","8","9"],"pause":".25" }'
+        #payload = '{ "state":"off", "order":["5","7","1","3","0","4","2","9","8","6"],"pause":".25" }'
 
         registry_manager.send_c2d_message(deviceId, payload, props)
     except Exception as ex:
