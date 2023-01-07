7365ee003113d76376b51315201cf89b772d767a
Jeff Beman <jbeman@microsoft.com>
2023-01-07 15:20:01 -0800

    refactored payload

--- python/c2dsendmsg.py
+++ python/c2dsendmsg.py
@@ -4,6 +4,36 @@ import json
 from uuid import uuid4
 from azure.iot.hub import IoTHubRegistryManager
 
+repeattime = 5
+repeatpause = 1
+pause = 0.5
+
+# Sample JSON for GMT 10:35 AM
+# { "repeat": 5, "repeatpause": 1, "pause": 0.5, "pm": "False", "time": [1,0,3,5]}
+def getpayload():
+    j = {}
+    j.update(repeat = repeattime)
+    j.update(repeatpause = repeatpause)
+    j.update(pause = pause)
+
+    t = []
+    hour = time.gmtime().tm_hour
+    
+    if hour > 12:
+        j.update(pm = 'True')
+        hour -= 12
+    else:
+        j.update(pm = 'False')
+
+    h = str(hour).zfill(2)
+    m = str(time.gmtime().tm_min).zfill(2)
+    t.append(int(h[0]))
+    t.append(int(h[1]))        
+    t.append(int(m[0]))
+    t.append(int(m[1]))
+    j.update(time = t)
+    return json.dumps(j)
+
 def main():
     deviceId = "raspberrypi2"
 
@@ -15,28 +45,7 @@ def main():
         props.update(messageId = "{0}".format(uuid4()))
         props.update(contentType = "application/json")
 
-        j = {}
-        j.update(repeat = 5)
-        j.update(repeatpause = 1)
-        j.update(pause = 0.5)
-        t = []
-        hour = time.gmtime().tm_hour
-        
-        if hour > 12:
-            j.update(pm = 'True')
-            hour -= 12
-        else:
-            j.update(pm = 'False')
-
-        h = str(hour).zfill(2)
-        m = str(time.gmtime().tm_min).zfill(2)
-        t.append(int(h[0]))
-        t.append(int(h[1]))        
-        t.append(int(m[0]))
-        t.append(int(m[1]))
-        j.update(time = t)
-        data = json.dumps(j)
-        registry_manager.send_c2d_message(deviceId, data, props)
+        registry_manager.send_c2d_message(deviceId, getpayload(), props)
     except Exception as ex:
         print ( "Unexpected error {0}" % ex )
 
