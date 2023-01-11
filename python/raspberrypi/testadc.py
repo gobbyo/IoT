8049f2c6a05cbe586dc5c7d8a660e8d078c4597f
Jeff Beman <jbeman@hotmail.com>
2023-01-10 21:22:34 -0800

    updated adc test

--- python/raspberrypi/testadc.py
+++ python/raspberrypi/testadc.py
@@ -1,18 +1,30 @@
 import modules.ADC0832 as RPI_ADC0832
+import time
 
-# Create an ADC0832 instance
-adc = RPI_ADC0832.ADC0832()
+def main():
 
-# Specify which GPIO pins will be used
-#adc.csPin = 17 # Default pin: 17
-#adc.clkPin = 27 # Default pin: 27
-#adc.doPin = 22 # Default pin: 23
-#adc.diPin = 22 # Default pin: 24
+    # Create an ADC0832 instance
+    adc = RPI_ADC0832.ADC0832()
 
-# Print the current value of channel 0
-valChannel0 = adc.read_adc(0)
-print('Value of channel 0: ' +  str(valChannel0))
+    # Specify which GPIO pins will be used
+    #adc.csPin = 17 # Default pin: 17
+    #adc.clkPin = 27 # Default pin: 27
+    #adc.doPin = 23 # Default pin: 23
+    #adc.diPin = 24 # Default pin: 24
+    prev = 0
+    
+    while True:
+        val = adc.read_adc(0)
+        ch0 = adc.read_adc(0)
+        ch1 = adc.read_adc(1)
+        if ch0 > ch1:
+            val = ch0
+        else:
+            val = ch1
+        if prev != val:
+            print('{0}'.format(val))
+            prev = val
+        time.sleep(.5)
 
-# Print the difference of both channel. You have to give which channel has the bigger value
-valDifference = adc.read_adc_difference(0)
-print('Difference: ' +  str(valDifference))
+if __name__ == "__main__":
+    main()
