1f72dc5a0a83354c754eb3db029cc34a76459ef9
gobbyo <jbeman@hotmail.com>
2023-01-27 23:13:32 -0800

    minor updates

--- python/raspberrypi/modules/raspipaddress.py
+++ python/raspberrypi/modules/raspipaddress.py
@@ -12,14 +12,8 @@ def get_ip_address():
         print("Error: {0}. CodeID = 7c931613-a37c-4631-9f30-ccfc3ed028f1".format(e))
     return ip_address
 
-def get_host_ip_address():
-    ip_address = ''
-    try:
-        host = socket.gethostname()
-        ip_address = socket.gethostbyname(host)
-    except socket.error as e:
-        print("Error: {0}. CodeID = 2e798e2d-0802-4b1d-9860-a83e3e35b599".format(e))
-    return ip_address
+def get_host_name():
+    return socket.gethostname()
 
 def get_public_address():
     return os.system('curl api.ipify.org')
