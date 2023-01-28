fba1dd2ddefe67543111fe074f03b6516c8048c2
gobbyo <jbeman@hotmail.com>
2023-01-27 22:54:52 -0800

    added new method

--- python/raspberrypi/modules/raspipaddress.py
+++ python/raspberrypi/modules/raspipaddress.py
@@ -4,6 +4,17 @@ import os
 def get_ip_address():
     ip_address = ''
     try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("1.1.1.1",80))
+        ip_address = s.getsockname()[0]
+        s.close()
+    except socket.error as e:
+        print("Error: {0}. CodeID = 7c931613-a37c-4631-9f30-ccfc3ed028f1".format(e))
+    return ip_address
+
+def get_host_ip_address():
+    ip_address = ''
+    try:
         host = socket.gethostname()
         ip_address = socket.gethostbyname(host)
     except socket.error as e:
