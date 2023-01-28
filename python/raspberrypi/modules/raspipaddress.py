95720f36f613978c3fb4107c045ebeabd16f38fe
Jeff Beman <jbeman@microsoft.com>
2023-01-27 22:32:01 -0800

    simplified getting ip address

--- python/raspberrypi/modules/raspipaddress.py
+++ python/raspberrypi/modules/raspipaddress.py
@@ -3,10 +3,8 @@ import socket
 def get_ip_address():
     ip_address = ''
     try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(("1.1.1.1",80))
-        ip_address = s.getsockname()[0]
-        s.close()
+        host = socket.gethostname()
+        ip_address = socket.gethostbyname(host)
     except socket.error as e:
         print("Error: {0}. CodeID = 2e798e2d-0802-4b1d-9860-a83e3e35b599".format(e))
     return ip_address
