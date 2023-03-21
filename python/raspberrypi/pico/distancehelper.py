9e7cbb861add2865e5c214c0628a14662d1b6985
Jeff Beman <jbeman@microsoft.com>
2023-03-20 17:07:51 -0700

    refined a few methods

--- python/raspberrypi/pico/distancehelper.py
+++ python/raspberrypi/pico/distancehelper.py
@@ -1,13 +1,8 @@
 cmtoinch = const(0.39370079)
 
 def getFeet(numInInches):
-    i = round(numInInches)
-    if i < 12:
-        return 0
-    else:
-        s = "{0}".format(i / 12)
-        i = s.find(".")
-        return (int(s[:i]))
+    s = "{0}".format(numInInches / 12)
+    return (int(s[:s.find(".")]))
     
 def getCentimeters(meters, totalCentimeters):
     return totalCentimeters - (meters * 100)
@@ -19,16 +14,11 @@ def getInches(feet, totalInches):
     return totalInches - (feet * 12)
 
 def getMeters(totalcentimeters):
-    i = round(totalcentimeters)
-    if i < 1:
-        return 
-    else:
-        s = "{0}".format(totalcentimeters/100)
-        i = s.find(".")
-        return (int(s[:i]))
+    s = "{0}".format(totalcentimeters / 100)
+    return int(s[:s.find(".")])
 
 def main():
-    totalCentimeters = 349.3245
+    totalCentimeters = 250.65
 
     meters = getMeters(totalCentimeters)
     print("meters = {0}".format(meters))
